3563f9cbfdf3e40aa4d15af63d3d943a052be752
07krW5Hnr5ghy <jryn145@gmail.com>
2025-01-28 00:23:04 -0500

    feature(main): add skills to the portfolio

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,6 +1,6 @@
 const Skill = ({ icon, skill }) => (
-    <div className="text-center">
-      <span className="text-4xl">{icon}</span>
+    <div className="text-center flex flex-col">
+      <div className="text-4xl mx-auto">{icon}</div>
       <p className="mt-2">{skill}</p>
     </div>
 );
