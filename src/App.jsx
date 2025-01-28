c21f88647fe09ad18403cd3a7ccad28831949a73
07krW5Hnr5ghy <jryn145@gmail.com>
2025-01-27 19:51:24 -0500

    feature(main): about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
+import About from './components/About'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className='min-h-screen flex flex-col'>
       <Header/>
       <Hero/>
+      <About/>
     </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = () => (
+    <section id="about" className="bg-gray-100 py-20">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold text-center mb-8">About Me</h2>
+        <p className="text-center max-w-2xl mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a diam et nisl suscipit gravida. Curabitur vel risus non erat varius cursus.</p>
+      </div>
+    </section>
+);
+
+export default About;
