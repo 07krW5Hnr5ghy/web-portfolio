53d7fb327a6abcd7f16819d365cea8a278db554d
07krW5Hnr5ghy <jryn145@gmail.com>
2025-01-29 20:37:15 -0500

    refactor(main): adjust profile photo in hero section

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,9 +1,19 @@
+import { FaGithub,FaLinkedin } from "react-icons/fa";
+
 const Footer = () => (
-    <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto text-center">
-        <p>&copy; 2025 YourName. All rights reserved.</p>
+  <footer className="bg-gray-800 text-white py-4">
+    <div className="container mx-auto text-center">
+      <p>&copy; 2025 YourName. All rights reserved.</p>
+      <div className="flex justify-center space-x-4 mt-2">
+        <a href="https://github.com/07krW5Hnr5ghy" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-gray-400">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/juan-pablo-romero-poveda-477514253/" target="_blank" rel="noopener noreferrer" className="text-white text-2xl hover:text-gray-400">
+          <FaLinkedin />
+        </a>
       </div>
-    </footer>
+    </div>
+  </footer>
 );
 
 export default Footer;
